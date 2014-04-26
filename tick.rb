212f8a71b3491f82b512c5712f664bf635955729
Travis Hohl <travis@travishohl.com>
2014-04-26 12:33:33 -0500

    Pass increment minutes test.

--- tick.rb
+++ tick.rb
@@ -1,6 +1,22 @@
-class Tick
+class Tick < Object
+
+	attr_accessor :hours
+	attr_accessor :minutes
+	attr_accessor :period
 	
 	def tock(string, increment)
-		string
+
+		# Parse the string using a regular expression
+		matches = string.match(/^(\d{1,2}):(\d{2}) (AM|PM)$/i)
+
+		# Assign matches to instance variables
+		@hours = matches[1].to_i
+		@minutes = matches[2].to_i
+		@period = matches[3]
+
+		# Perform math on minutes
+		@minutes += increment
+
+		return "#{@hours}:#{@minutes} #{@period}"
 	end
 end
