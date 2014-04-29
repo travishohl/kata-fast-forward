b2c08ea0ba95b0044a564d1095c6aa3f6780c746
Travis Hohl <travis@travishohl.com>
2014-04-29 07:43:52 -0500

    Test whether the period (AM/PM) flips when hours become greater than 11.

--- tick_spec.rb
+++ tick_spec.rb
@@ -20,4 +20,9 @@ describe Tick, "#tock" do
 		result = @tick.tock("9:13 AM", 47)
 		expect(result).to eql("10:00 AM")
 	end
+
+	it "should flip the period (AM/PM) when hours are greater than 11" do
+		result = @tick.tock("11:21 AM", 39)
+		expect(result).to eql("12:00 PM")
+	end
 end
