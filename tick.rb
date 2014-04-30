5e3c8566d7c4dea76469fd4ae30ba663b5f7005e
Travis Hohl <travis@travishohl.com>
2014-04-29 20:30:19 -0500

    Pass test: whether AM/PM are flipped when hours > 11

--- tick.rb
+++ tick.rb
@@ -14,14 +14,27 @@ class Tick < Object
 		@minutes = matches[2].to_i
 		@period = matches[3]
 
-		# Perform math
+		# Increment minutes
 		@minutes += increment
 
+		# Increment hours, if necessary
 		if @minutes > 59
 			@hours += (@minutes / 60)
 			@minutes = (@minutes % 60)
+
+			# Swap am and pm, if necessary
+			swaps = (@hours / 12)
+			swap_am_pm unless swaps.even?
 		end
 
 		return "#{@hours}:#{"%02d" % @minutes} #{@period}"
+	end
+
+	def swap_am_pm
+		if (@period == "AM")
+			@period = "PM"
+		else
+			@period = "AM"
+		end
 	end
 end
