5444a2e1b8efa281988ded7b4411bcac34ecc93b
Travis Hohl <travis@travishohl.com>
2014-05-06 20:44:51 -0500

    Add two new tests.
    One already passes though, so focus only on the second test.

--- tick_spec.rb
+++ tick_spec.rb
@@ -17,8 +17,18 @@ describe Tick, "#tock" do
 		expect(result).to eql("10:00 AM")
 	end
 
-	it "should flip the period (AM/PM) when hours are greater than 11" do
+	it "should flip the period from AM to PM when hours are greater than 11 when starting at AM" do
 		result = Tick.tock("11:21 AM", 39)
 		expect(result).to eql("12:00 PM")
+	end
+
+	it "should flip the period from PM to AM when hours are greater than 11 when starting at PM" do
+		result = Tick.tock("11:21 PM", 39)
+		expect(result).to eql("12:00 AM")
+	end
+
+	it "should do nothing when increment is 1440 (24 hours)" do
+		result = Tick.tock("1:37 PM", 1440)
+		expect(result).to eql("1:37 PM")
 	end
 end
